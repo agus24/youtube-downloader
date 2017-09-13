5f0122eb73296d4dd0089b27cf41696af8738e02
agus24 <=>
2017-09-13 13:49:03 +0700

    install

---
+++ index.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var youtubedl = require('youtube-dl');
+
+var format = "";
+var argv = process.argv.slice(2);
+if(argv[0] == "audio") {
+  format = "m4a";
+}
+else if(argv[0] == "video") {
+  format = "mp4";
+}
+
+var urls = argv[1];
+var dir = checkDir("music/"+argv[2]+"/");
+
+playlist(urls);
+function playlist(url) {
+    'use strict';
+    var video = youtubedl(url, ["-f "+format], { cwd: __dirname });
+
+    var size = 0;
+    video.on('info', function(info) {
+      console.log('Download started');
+      console.log('filename: ' + info._filename);
+      console.log('size: ' + info.size);
+        video.pipe(fs.createWriteStream(dir+info._filename, { flags: 'a' }));
+        size = info.size;
+    });
+
+    var pos = 0;
+    video.on('data', function data(chunk) {
+        pos += chunk.length;
+        // `size` should not be 0 here.
+        if (size) {
+          var percent = (pos / size * 100).toFixed(2);
+          process.stdout.cursorTo(0);
+          process.stdout.clearLine(1);
+          process.stdout.write(percent + '%(' + pos + "/"+size+")");
+        }
+    });
+
+    video.on('next', playlist)
+}
+
+function checkDir(directory) {
+  if (!fs.existsSync(directory)){
+    fs.mkdirSync(directory);
+  }
+  return directory;
+}
